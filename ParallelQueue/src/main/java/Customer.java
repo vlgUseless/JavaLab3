bdc605b2e3856758cfd98731fe9fdb03559fa55f
Vladislav Gaar <gaarvlad@gmail.com>
2024-12-17 22:59:21 +0300

    Implemented run method in Customer class

--- ParallelQueue/src/main/java/Customer.java
+++ ParallelQueue/src/main/java/Customer.java
@@ -8,5 +8,18 @@ public class Customer implements Runnable {
     }
 
     @Override
-    public void run() {}
+    public void run() {
+        try {
+            // Ждем, пока очередь дойдет до нашего билета
+            system.waitForTurn(ticket);
+            // Имитация обслуживания
+            System.out.println("Customer with ticket " + ticket + " is being served.");
+            Thread.sleep((long) (Math.random() * 1000));
+            System.out.println("Customer with ticket " + ticket + " has been served.");
+            // Сообщаем системе о завершении обслуживания
+            system.done();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
